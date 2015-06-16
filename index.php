7ffeb8b5fe0e7d1faf1b63a82d6b498abbe34f8f
Etoma <etienne.scherrer@hotmail.com>
2015-06-16 08:41:05 +0200

    deleted Zend

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
-<?php 
-define('RUNNING_FROM_ROOT', true);
-include 'public/index.php';
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>My Movie and Audio Page</title>
+</head>
+<body>
+	<nav>
+		<div class="container">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="index.phtml">Home</a></li>
+				<li class="active"><a href="index.phtml">Songs</a></li>
+				<li class="active"><a href="index.phtml">Movies</a></li>
+			</ul>
+		</div>
+	</nav>
+	<div class="container">
+		//ToDo replace with variable for current content
+		<?php require("../view/index.pthml") ?>
+		<hr>
+		<footer>
+			<p>footer</p>
+		</footer>
+	</div>
+</body>
+</html>
